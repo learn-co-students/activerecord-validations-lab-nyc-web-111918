e76f15435a7d3493738841b9626d5e4c8fc1b05a
aschemily <aschemily@gmail.com>
2018-12-17 17:39:22 -0500

    Done.

---
+++ app/validators/title_validator.rb
@@ -0,0 +1,11 @@
+# class TitleValidator < ActiveModel::EachValidator
+#   def validate_clickbait
+#
+# #words = ["Won't Believe", "Secret", "Top", "Guess"]
+# if !title.is_a?(Array) || wdays.detect{|d| !(0..6).include?(d)}
+#     errors.add(:wdays, :invalid)
+#   end
+#
+#
+#   end
+# end
